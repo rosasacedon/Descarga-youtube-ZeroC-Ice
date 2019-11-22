f9511eda9bc9a7ffc42c4ab935149621f7c51730
Rosa María Sacedón Ortega <rosasacedon19@gmail.com>
2019-11-22 20:13:27 +0100

    Actualizacion del <run_client.sh>

--- run_client.sh
+++ run_client.sh
@@ -1,3 +1,11 @@
 #!/bin/sh
 
+echo "Running client"
+
+#nombre_interprete_python nombre_clase.py "<proxy_orchestrator>" "<url>" 
+#./client.py $1 $2
+#Ejecucion segun el interprete python3 cliente.py "proxy entre comillas" "youtube.com"
+#./cliente.py $1 $2
+
+#Se ejecuta ./run_client.sh "<proxy_orchestrator>" "<url>" 
 ./client.py "$1" "$2"
