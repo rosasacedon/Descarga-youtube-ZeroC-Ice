38b86403a8a72225068b7182b050a504dce370dc
Rosa María Sacedón Ortega <rosasacedon19@gmail.com>
2019-12-09 11:43:26 +0100

    Actualizacion <run_server.sh> para segunda entrega

--- run_server.sh
+++ run_server.sh
@@ -1,11 +1,27 @@
 #!/bin/sh
 
+#Ejecutar IceBox
+rm -r IceStorm/
 
-./downloader.py --Ice.Config=server.config | tee proxy_downloader.out &
+mkdir -p IceStorm/
 
 sleep 2
 
-./orchestrator.py --Ice.Config=orchestrator.config "$(head -1 proxy_downloader.out)"
+sudo icebox --Ice.Config=icebox.config &
+
+sleep 2
+
+./downloader.py --Ice.Config=server.config | tee proxy.out &
+
+sleep 2
+
+prx=$(head -1 proxy.out)
+
+./orchestrator.py --Ice.Config=server.config "$prx" &
+
+sleep 2
+
+./orchestrator.py --Ice.Config=server.config "$prx"
 
 
 
