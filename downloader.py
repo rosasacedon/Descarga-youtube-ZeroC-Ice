ca87ecbda45b9903860ab83757534efc1c463860
Riku97 <vic.sax.29@gmail.com>
2019-12-09 10:32:56 +0100

    Subida del archivo downloader.py

--- downloader.py
+++ downloader.py
@@ -7,22 +7,71 @@ Downloader
 
 import sys
 import os.path
+import hashlib
 import youtube_dl #pylint: disable=E0401
 import Ice # pylint: disable=E0401,E0401
+import IceStorm
 Ice.loadSlice('trawlnet.ice')
 import TrawlNet # pylint: disable=E0401,C0413
 
 
-class Downloader(TrawlNet.Downloader):  # pylint: disable=R0903
+class DownloaderI(TrawlNet.Downloader):  # pylint: disable=R0903
     '''
     Downloader
     '''
+     publisher = None
+
+    def __init__(self, topic):
+        self.topic = topic
+    
     def addDownloadTask(self, url, current=None): # pylint: disable=C0103, R0201, W0613
         '''
         Task
         '''
-        print(url)
-        return download_mp3(url)
+        fileSystem = download_mp3(url)
+        fileInfo = TrawlNet.FileInfo()
+        fileInfo.name = os.path.basename(fileSystem)
+        fileInfo.hash = hash_this(fileInfo.name)
+        orchestrators = self.topic.getPublisher()
+        object_orchestrator_event = TrawlNet.UpdateEventPrx.uncheckedCast(orchestrators)
+        object_orchestrator_event.newFile(fileInfo)
+        return fileInfo
+
+class Server(Ice.Application): # pylint: disable=R0903
+    '''
+    Server instance
+    '''
+    def get_topic_manager(self):
+        key = 'IceStorm.TopicManager.Proxy'
+        proxy = self.communicator().propertyToProxy(key)
+        if proxy is None:
+            return None
+
+        return IceStorm.TopicManagerPrx.checkedCast(proxy)
+
+    def run(self, argv): # pylint: disable=W0613
+        '''
+        Run Server
+        '''
+        topic_mgr = self.get_topic_manager()
+        if not topic_mgr:
+            return 2
+
+        topic_name = "UpdateEvents"
+        try:
+            topic = topic_mgr.retrieve(topic_name)
+        except IceStorm.NoSuchTopic:
+            topic = topic_mgr.create(topic_name)
+
+        servant = DownloaderI(topic)
+        broker = self.communicator()
+        adapter = broker.createObjectAdapter("DownloaderAdapter")
+        prox = adapter.add(servant, broker.stringToIdentity("downloader"))
+        print(prox, flush=True)
+        adapter.activate()
+        self.shutdownOnInterrupt()
+        broker.waitForShutdown()
+        return 0
 
 class NullLogger:
     '''
@@ -80,25 +129,13 @@ def download_mp3(url, destination='./music/'):
     filename = filename[:filename.rindex('.') + 1]
     return filename + options['postprocessors'][0]['preferredcodec']
 
+def hash_this(filename):
+    fileHash = hashlib.sha256()
+    with open(filename, "rb") as f:
+        for chunk in iter(lambda: f.read(4096), b''):
+            fileHash.update(chunk)
+    return fileHash.hexdigest()
 
-class Servidor(Ice.Application): # pylint: disable=R0903
-    '''
-    Servidor
-    '''
 
-    def run(self, argv): # pylint: disable=W0613
-        '''
-        Iniciar servidor
-        '''
-        serv = Downloader()
-        broker = self.communicator()
-        adapter = broker.createObjectAdapter("DownloaderAdapter")
-        dwn = adapter.add(serv, broker.stringToIdentity("dl"))
-        print(dwn, flush=True)
-        adapter.activate()
-        self.shutdownOnInterrupt()
-        broker.waitForShutdown()
-        return 0
-
-SERV = Servidor()
-sys.exit(SERV.main(sys.argv))
+SERVER_DOWN = Server()
+sys.exit(SERVER_DOWN.main(sys.argv))
