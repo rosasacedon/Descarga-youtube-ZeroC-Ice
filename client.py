8adbe5d0bf48e2138912f697cc0d88a5d53a4f11
riku97 <victor.martin.alonso1@gmail.com>
2020-01-20 00:42:52 +0100

    Cliente actualizado

--- client.py
+++ client.py
@@ -5,52 +5,45 @@ Implementacion cliente
 '''
 import os
 import binascii
-
 import sys
-import Ice # pylint: disable=E0401,E0401
+import Ice # pylint: disable=E0401
 Ice.loadSlice('trawlnet.ice')
 import TrawlNet # pylint: disable=E0401,C0413
 
-APP_DIRECTORY = './'
-DOWNLOADS_DIRECTORY = os.path.join(APP_DIRECTORY, 'downloads')
+DOWNLOADS_DIRECTORY = './'
 
-class Client(Ice.Application):
+class Client(Ice.Application): # pylint: disable=R0903
     '''
     Clase cliente
     '''
-    def descargar_cancion(self, orchestrator, url, current=None):
-        ''' Descargar cancion '''
-        orchestrator.downloadTask(url)
- 
-
-    def get_list(self, orchestrator, current=None):
-        ''' obtener lista '''
-        lista = []
-        lista = orchestrator.getFileList()
-        print(lista)
-
+  
     def run(self, argv):
         ''' Run '''
+
         proxy = self.communicator().stringToProxy(argv[1])
         orchestrator = TrawlNet.OrchestratorPrx.checkedCast(proxy)
+
         if not orchestrator:
-            raise RuntimeError("Invalid proxy")
+            raise RuntimeError('Invalid orchestrator proxy')
         
         if len(argv) == 2:
-            self.get_list(orchestrator)
-            sys.exit()
+            print('Lista de canciones preparadas')
+            print(orchestrator.getFileList())
+            sys.exit
+	elif len(argv) == 4:
+            if argv[2] == '--download':
+                orchestrator.downloadTask(argv[3])
+            elif argv[2] == '--transfer':
+                self.transfer_request(argv[3], orchestrator)
         
-        self.descargar_cancion(orchestrator, argv[2])
+        return 0
 
-
-
-
-def transfer_request(self, file_name):
+def transfer_request(self, file_name, orchestrator):
     remote_EOF = False
     BLOCK_SIZE = 1024
     transfer = None
     try:
-        transfer = self.orchestrator.getFile(file_name)
+        transfer = orchestrator.getFile(file_name)
     except TrawlNet.TransferError as e:
         print(e.reason)
         return 1
